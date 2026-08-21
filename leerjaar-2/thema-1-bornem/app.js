edc32501fd01d945500f2ceb2a77399a57f680be
IsabelRockele <rockele@msn.com>
2026-08-21 19:18:49 +0200

    Vervang zwevende illustraties door levensechte foto's

--- leerjaar-2/thema-1-bornem/app.js
+++ leerjaar-2/thema-1-bornem/app.js
@@ -56,9 +56,9 @@ const picturePairs=[
  {w:'het adres',img:'adres-illustratie.jpg'},{w:'het afval',img:'afval-illustratie.jpg'},{w:'hergebruiken',img:'hergebruiken-illustratie.jpg'},{w:'recycleren',img:'recycleren-illustratie.jpg'},
  {w:'afval verbranden',img:'verbranden-illustratie.jpg'},{w:'afval voorkomen',img:'voorkomen-illustratie.jpg'},{w:'de akkerbouw',img:'akkerbouw-illustratie.jpg'},{w:'het bestuur',img:'bestuur-illustratie.jpg'},
  {w:'de burgemeester',img:'burgemeester-greet-de-bruyn.png'},{w:'de cultuur',img:'ter-dilft.jpg'},{w:'de dienst',img:'brandweer-bornem.jpg'},{w:'het gemeentehuis',img:'gemeentehuis-bornem.jpg'},
- {w:'de gids',img:'gids-illustratie.jpg'},{w:'de glastuinbouw',img:'glastuinbouw-illustratie.jpg'},{w:'de handel',img:'winkelstraat-bornem.jpg'},{w:'het industriegebied',img:'industriegebied-illustratie.jpg'},
- {w:'het landelijk landschap',img:'landelijk-illustratie.jpg'},{w:'de legende',img:'legende-illustratie.jpg'},{w:'de luchtfoto',img:'luchtfoto-school.png'},{w:'het natuurgebied',img:'natuurgebied-illustratie.jpg'},
- {w:'het recreatiegebied',img:'breeven.jpg'},{w:'het stedelijk landschap',img:'stedelijk-illustratie.jpg'},{w:'het stratenplan',img:'stratenplan-school.png'},{w:'het openbaar vervoer',img:'station-bornem.jpg'}
+ {w:'de gids',img:'gids-illustratie.jpg'},{w:'de glastuinbouw',img:'landbouw-glastuinbouw-foto.webp'},{w:'de handel',img:'winkelstraat-bornem.jpg'},{w:'het industriegebied',img:'gebied-industriegebied-foto-v2.webp'},
+ {w:'het landelijk landschap',img:'landschap-kasteel-bornem.webp'},{w:'de legende',img:'legende-kaart-uitgesneden.webp'},{w:'de luchtfoto',img:'luchtfoto-school.png'},{w:'het natuurgebied',img:'gebied-natuurgebied-foto-v2.webp'},
+ {w:'het recreatiegebied',img:'breeven.jpg'},{w:'het stedelijk landschap',img:'landschap-stad-stedelijk-2.webp'},{w:'het stratenplan',img:'stratenplan-school.png'},{w:'het openbaar vervoer',img:'station-bornem.jpg'}
 ];
 const homeOptions=[
  {key:'flat',label:'de flat',img:'flat-eigen.webp'},
